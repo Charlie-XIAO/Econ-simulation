f442865ed0e4b69d192289c5c990477c62952363
Charlie-XIAO <yx2436@nyu.edu>
2022-12-11 10:07:52 +0800

    updated supply and demand market methods, specified function and methods parameter and return types, added descriptions

--- src/main/agent.py
+++ src/main/agent.py
@@ -1,168 +1,217 @@
 from abc import ABC, abstractmethod
 
 import random
-import numpy as np
 
 UNIT_PER_DAY = 5
 
 class Agent(ABC):
 
-    def __init__(self, food, wood, ore, metal, tool):
+    def __init__(self, food:float, wood:float, ore:float, metal:float, tool:float):
+        """
+        :param food: the initial number of food the agent has
+        :param wood: the initial number of wood the agent has
+        :param ore: the initial number of ore the agent has
+        :param metal: the initial number of metal the agent has
+        :param tool: the initial number of tool the agent has
+        """
         self.id = random.randint(100000, 999999)
         self.alive = 1
         self.inventory = {"food": food, "wood": wood, "ore": ore, "metal": metal, "tool": tool}
 
     @abstractmethod
     def unit_work(self) -> None:
+        """
+        Description: the working behavior of the agent per unit working time
+        """
         pass
 
-    def work(self):
+    def work(self) -> None:
+        """
+        Description: the behavior of the agent per day
+        """
         for _ in range(UNIT_PER_DAY):
             self.unit_work()
         assert(self.inventory["food"] >= 8)
         self.consume("food", 8)
 
-    def die(self):
+    def die(self, reason:str) -> None:
+        """
+        :param reason: the reason that the agent dies
+        Description: kill the agent and report death due to the specified reason
+        """
         self.alive = 0
-        print("{} {}: died of starvation", self.__class__.__name__, self.id)
+        print("{} {}: died of {}", self.__class__.__name__, self.id, reason)
     
 
-    def consume(self, comodity, n):
-        assert(comodity in ["food", "wood", "ore", "metal"])
+    def consume(self, comodity:str, n:float) -> None:
+        """
+        :param comodity: the comodity the agent consumes
+        :param n: the number of the specified comodity the agent consumes
+        Description: consume the specified number of the specified commodity
+        """
         self.inventory[comodity] -= n
-    
-    def has_tool(self):
-        return self.inventory["tool"] >= 1
-    
-    def break_tool(self, prob):
-        if (random.random() < prob):
-            self.inventory["tool"] -= 1
 
-    def produce(self, comodity, n):
+    def produce(self, comodity:str, n:float) -> None:
+        """
+        :param comodity: the comodity the agent produces
+        :param n: the number of the specified comodity the agent produces
+        Description: produce the specified number of the specified commodity
+        """
         assert(self.inventory[comodity] >= n)
         self.inventory[comodity] += n
     
     @abstractmethod
-    def surplus(self) -> dict:
+    def surplus(self) -> dict[str, float]:
+        """
+        :return: the surplus for each comodity of the agent for exchange
+        """
         return {comodity: 0 for comodity in ["food", "wood", "ore", "metal", "tool"]}
 
     @abstractmethod
-    def shortage(self) -> dict:
+    def shortage(self) -> dict[str, float]:
+        """
+        :return: the shortage of each comodity of the agent to exchange
+        """
         return {comodity: 0 for comodity in ["food", "wood", "ore", "metal", "tool"]}
 
 class Farmer(Agent):
 
+    # Overwrite initialization method
     def __init__(self, food, wood, ore, metal, tool):
         super().__init__(food, wood, ore, metal, tool)
 
+    # Overwrite abstract method
     def unit_work(self):
-        if self.has_tool():
+        if self.inventory["tool"] >= 0.03:
             source = max(self.inventory["wood"], 4)
             self.consume("wood", source)
+            self.consume("tool", 0.03)
             self.produce("food", 2 * source)
-            self.break_tool(0.03)
         else:
             source = max(self.inventory["food"], 4)
             self.consume("wood", source)
             self.produce("food", source)
     
+    # Overwrite abstract method
     def shortage(self):
-        shortage = {comodity: 0 for comodity in ["food", "ore", "metal"]}
+        shortage = {comodity: float(0) for comodity in ["food", "ore", "metal"]}
         shortage["wood"] = 4 * UNIT_PER_DAY - self.inventory["wood"]
-        shortage["tool"] = 1 - self.inventory["tool"]
+        shortage["tool"] = 0.03 * UNIT_PER_DAY - self.inventory["tool"]
         return shortage
 
+    # Overwrite abstract method
     def surplus(self):
-        surplus = {comodity: 0 for comodity in ["wood", "ore", "metal", "tool"]}
+        surplus = {comodity: float(0) for comodity in ["wood", "ore", "metal", "tool"]}
         surplus["food"] = self.inventory["food"] - 8
+        return surplus
     
 class WoodCutter(Agent):
 
+    # Overwrite initialization method
     def __init__(self, food, wood, ore, metal, tool):
         super().__init__(food, wood, ore, metal, tool)
     
+    # Overwrite abstract method
     def unit_work(self):
-        if self.has_tool():
+        if self.inventory["tool"] >= 0.03:
+            self.consume("tool", 0.03)
             self.produce("wood", 4)
-            self.break_tool(0.03)
         else:
             self.produce("wood", 2)
     
+    # Overwrite abstract method
     def shortage(self):
-        shortage = {comodity: 0 for comodity in ["wood", "ore", "metal"]}
+        shortage = {comodity: float(0) for comodity in ["wood", "ore", "metal"]}
         shortage["food"] = 8 - self.inventory["food"]
-        shortage["tool"] = 1 - self.inventory["tool"]
+        shortage["tool"] = 0.03 * UNIT_PER_DAY - self.inventory["tool"]
         return shortage
 
+    # Overwrite abstract method
     def surplus(self):
-        surplus = {comodity: 0 for comodity in ["food", "ore", "metal", "tool"]}
+        surplus = {comodity: float(0) for comodity in ["food", "ore", "metal", "tool"]}
         surplus["wood"] = self.inventory["wood"]
+        return surplus
 
 class Miner(Agent):
 
+    # Overwrite initialization method
     def __init__(self, food, wood, ore, metal, tool):
         super().__init__(food, wood, ore, metal, tool)
     
+    # Overwrite abstract method
     def unit_work(self):
-        if self.has_tool():
+        if self.inventory["tool"] >= 0.05:
+            self.consume("tool", 0.05)
             self.produce("ore", 8)
-            self.break_tool(0.05)
         else:
             self.produce("ore", 4)
     
+    # Overwrite abstract method
     def shortage(self):
-        shortage = {comodity: 0 for comodity in ["wood", "ore", "metal"]}
+        shortage = {comodity: float(0) for comodity in ["wood", "ore", "metal"]}
         shortage["food"] = 8 - self.inventory["food"]
-        shortage["tool"] = 1 - self.inventory["tool"]
+        shortage["tool"] = 0.05 * UNIT_PER_DAY - self.inventory["tool"]
         return shortage
 
+    # Overwrite abstract method
     def surplus(self):
-        surplus = {comodity: 0 for comodity in ["food", "wood", "metal", "tool"]}
+        surplus = {comodity: float(0) for comodity in ["food", "wood", "metal", "tool"]}
         surplus["ore"] = self.inventory["ore"]
+        return surplus
 
 class Refiner(Agent):
 
+    # Overwrite initialization method
     def __init__(self, food, wood, ore, metal, tool):
         super().__init__(food, wood, ore, metal, tool)
     
+    # Overwrite abstract method
     def unit_work(self):
-        if self.has_tool():
+        if self.inventory["tool"] >= 0.05:
             source = max(self.inventory["ore"], 2)
             self.consume("ore", source)
+            self.consume("tool", 0.05)
             self.produce("metal", source)
-            self.break_tool(0.05)
         else:
             source = max(self.inventory["ore"], 4) // 2 * 2
             self.consume("ore", source)
             self.produce("metal", source // 2)
-        
+    
+    # Overwrite abstract method
     def shortage(self):
-        shortage = {comodity: 0 for comodity in ["wood", "metal"]}
+        shortage = {comodity: float(0) for comodity in ["wood", "metal"]}
         shortage["food"] = 8 - self.inventory["food"]
         shortage["ore"] = 2 * UNIT_PER_DAY - self.inventory["ore"]
-        shortage["tool"] = 1 - self.inventory["tool"]
+        shortage["tool"] = 0.05 * UNIT_PER_DAY - self.inventory["tool"]
         return shortage
 
+    # Overwrite abstract method
     def surplus(self):
-        surplus = {comodity: 0 for comodity in ["food", "wood", "ore", "tool"]}
+        surplus = {comodity: float(0) for comodity in ["food", "wood", "ore", "tool"]}
         surplus["metal"] = self.inventory["metal"]
+        return surplus
     
 class BlackSmith(Agent):
 
+    # Overwrite initialization method
     def __init__(self, food, wood, ore, metal, tool):
         super().__init__(food, wood, ore, metal, tool)
     
+    # Overwrite abstract method
     def unit_work(self):
         if self.inventory["metal"] >= 2:
             self.consume("metal", 2)
             self.produce("tool", 1)
 
+    # Overwrite abstract method
     def shortage(self):
-        shortage = {comodity: 0 for comodity in ["wood", "ore", "tool"]}
+        shortage = {comodity: float(0) for comodity in ["wood", "ore", "tool"]}
         shortage["food"] = 8 - self.inventory["food"]
         shortage["metal"] = 2 * UNIT_PER_DAY - self.inventory["metal"]
         return shortage
 
+    # Overwrite abstract method
     def surplus(self):
-        surplus = {comodity: 0 for comodity in ["food", "wood", "ore", "metal"]}
+        surplus = {comodity: float(0) for comodity in ["food", "wood", "ore", "metal"]}
         surplus["tool"] = self.inventory["tool"]
+        return surplus
--- src/main/market.py
+++ src/main/market.py
@@ -1,20 +1,38 @@
 from abc import ABC, abstractmethod
+from agent import *
 
+import random
 import numpy as np
 
 class Market(ABC):
 
-    def __init__(self, agents, prices):
+    def __init__(self, agents:list[Agent], prices:dict[str, float]) -> None:
+        """
+        :param agents: the list of agents
+        :param prices: the dictionary with keys as comodities and values as the corresponding initial prices
+        """
         self.price_history = {comodity: [prices[comodity]] for comodity in prices.keys()}
         self.agents = agents
     
-    def agents_alive(self):
-        return [agent for agent in self.agents if agent.alive == 1]
-    
-    def cur_price(self, comodity):
+    def cur_price(self, comodity:str) -> float:
+        """
+        :param commodity: the commodity to obtain the current price
+        :return: the current price of the specified commodity
+        """
         return self.price_history[comodity][-1]
+    
+    def update_price_history(self, prices:dict[str, float]) -> None:
+        """
+        Description: updates the price history
+        :param prices: the dictionary with keys as commodities and values as the corresponding prices to update
+        """
+        for comodity in self.price_history.keys():
+            self.price_history[comodity].append(prices[comodity])
 
-    def demand_matrix(self):
+    def demand_matrix(self) -> list[np.ndarray]:
+        """
+        :return: the demand matrix, with each commodity (food, wood, ore, metal, tool) as a column and each agent as a row
+        """
         demand_matrix = []
         for agent in self.agents:
             demand_matrix.append(np.array(
@@ -26,7 +44,17 @@ class Market(ABC):
             ))
         return demand_matrix
     
-    def supply_matrix(self):
+    def total_demand(self, comodity:str) -> float:
+        """
+        :param comodity: the commodity to obtain the total demand
+        :return: the total demand of the specified commodity
+        """
+        return sum([agent.shortage()[comodity] for agent in self.agents])
+    
+    def supply_matrix(self) -> list[np.ndarray]:
+        """
+        :return: the supply matrix, with each commodity (food, wood, ore, metal, tool) as a column and each agent as a row
+        """
         supply_matrix = []
         for agent in self.agents:
             supply_matrix.append(np.array(
@@ -37,44 +65,111 @@ class Market(ABC):
                 agent.surplus()["tool"],
             ))
         return supply_matrix
-    
-    def take_all_production(self):
-        pass
 
-    def satisfy_demand(self, r):
-        pass
+    def total_supply(self, comodity:str) -> float:
+        """
+        :param comodity: the commodity to obtain the total supply
+        :return: the total supply of the specified commodity
+        """
+        return sum([agent.surplus()[comodity] for agent in self.agents])
+    
+    def take_supply(self) -> dict[str, float]:
+        """
+        Description: takes away all the supplies from all the agents
+        """
+        total_supply = {comodity: float(0) for comodity in ["food", "wood", "ore", "metal", "tool"]}
+        for agent in self.agents:
+            for comodity in agent.surplus().keys():
+                if agent.surplus()[comodity] > 0:
+                    total_supply[comodity] += agent.inventory[comodity]
+                    agent.inventory[comodity] = 0
+        return total_supply
+
+    def satisfy_demand(self, r:np.ndarray) -> None:
+        """
+        Description: gives each agent some commodities according to the assigned intensity and their demand
+        :param r: the intensity assigned for each agent (same order as in `self.agents`)
+        """
+        for agent in self.agents:
+            for comodity in agent.shortage().keys():
+                agent.inventory[comodity] += agent.shortage()[comodity] * r
     
     @abstractmethod
     def update_price(self) -> None:
+        """
+        Description: updates the price with some market strategy, and update the price history
         # Walrasian: solve ODE with constraint
         # Supply and demand: adjust by some percentage of the ratio of supply versus demand
+        """
         pass
 
     @abstractmethod
     def distribute(self) -> None:
+        """
+        Description: distributes the supply and demand with some market strategy, according to the updated price
         # Walrasian: obtain r with ODE result, take away everything one produces, give r times everything one demands
         # Supply and demand: take away everything one produces, give each agent corresponding money, with some random order,
         #                    the agents use those money to buy as many times everything they demand as they can
+        """
         pass
 
 class WalrasianMarket(Market):
 
+    # Overwrite initialization method
     def __init__(self, agents, prices):
         super().__init__(agents, prices)
     
+    # Overwrite abstract method
     def update_price(self):
         pass
 
+    # Overwrite abstract method
     def distribute(self):
         pass
 
 class SupplyDemandMarket(Market):
 
+    # Overwrite initialization method
     def __init__(self, agents, prices):
         super().__init__(agents, prices)
     
+    # Overwrite abstract method
     def update_price(self):
-        pass
+        # Create a new price based on the total supply and demand
+        new_prices = {comodity: float(0) for comodity in ["food", "wood", "ore", "metal", "tool"]}
+        for comodity in new_prices.keys():
+            ratio = self.total_demand(comodity) / self.total_supply(comodity)
+            if ratio > 1:
+                new_prices[comodity] += self.cur_price(comodity) * ratio * 0.03
+            else:
+                new_prices[comodity] -= self.cur_price(comodity) / ratio * 0.03
+        # Update the price history and take away all the supplies from all agents
+        self.update_price_history(new_prices)
+        self.take_supply()
 
+    # Overwrite abstract method
     def distribute(self):
-        pass
+        # Distribute wealth to each agent based on the current prices and their surplus,
+        # meanwhile collecting the commodities they are trying to sell
+        wealth = []
+        collected = {comodity: 0 for comodity in ["food", "wood", "ore", "metal", "tool"]}
+        prices = [self.cur_price(comodity) for comodity in ["food", "wood", "ore", "metal", "tool"]]
+        for agent in self.agents:
+            surpluses = []
+            for comodity in ["food", "wood", "ore", "metal", "tool"]:
+                surpluses.append(agent.surplus()[comodity])
+                collected[comodity] += surpluses[-1]
+            wealth.append(np.dot(prices, surpluses))
+        # Each agent tries to spend all of their wealth for what they demand (in certain proportion),
+        # note that the last ones to trade may not get what they want, meaning that they receive less value that they give
+        order = list(range(len(self.agents)))
+        random.shuffle(order)
+        for i in order:
+            cur_agent = self.agents[i]
+            demands = [cur_agent.shortage()[comodity] for comodity in ["food", "wood", "ore", "metal", "tool"]]
+            r = wealth[i] / np.dot(prices, demands)
+            for comodity in ["food", "wood", "ore", "metal", "tool"]:
+                exchange_amount = min(r * cur_agent.shortage()[comodity], collected[comodity])
+                cur_agent.inventory[comodity] += exchange_amount
+                collected[comodity] -= exchange_amount
+
